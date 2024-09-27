000e4f76edb5a1dd2182baa92f8373da014cc9cd
김대호 <kimdaeho@gimdaehoui-MacBookPro.local>
2024-09-27 16:48:30 +0900

    jakarta

--- src/main/java/com/autocrypt/logtracer/v3/TestEntity.java
+++ src/main/java/com/autocrypt/logtracer/v3/TestEntity.java
@@ -1,10 +1,10 @@
 package com.autocrypt.logtracer.v3;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Getter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
 @Entity
 @Getter

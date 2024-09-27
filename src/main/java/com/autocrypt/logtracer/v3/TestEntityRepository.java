4a68841f444de3a52e97cb016d6b08f8b8f5c838
김대호 <kimdaeho@gimdaehoui-MacBookPro.local>
2024-09-27 16:44:40 +0900

    jpa aop

---
+++ src/main/java/com/autocrypt/logtracer/v3/TestEntityRepository.java
@@ -0,0 +1,6 @@
+package com.autocrypt.logtracer.v3;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestEntityRepository extends JpaRepository<TestEntity, Long> {
+}
